dd513b76242d307343ac7d8e6b0d2014bb2639b5
MariiaSheinyk <mariasheynik@gmail.com>
2024-01-17 13:12:08 +0100

    change path in router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import SimpleMovingButton from "./components/Buttons/SimpleMovingButton/SimpleMo
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const router = createBrowserRouter(routes, { basename: "/pet-project" });
+const router = createBrowserRouter(routes, {
+  basename: "/ms3012-91.github.io/VNV-Solutions",
+});
 
 root.render(
   <React.StrictMode>
